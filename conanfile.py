7fb92a932ee13dd015df0cf1e0f5944e79bddb42
lilAbi <105619619+lilAbi@users.noreply.github.com>
2024-11-25 18:04:05 -0600

    got it working on windows

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,13 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import copy
 
-
 class ImGuiExample(ConanFile):
     description = "ConanFile for IRC"
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
         self.requires("spdlog/1.13.0")
-        self.requires("boost/1.86.0")
+        self.requires("boost/1.85.0")
 
     def generate(self):
         toolchain = CMakeToolchain(self)
@@ -19,6 +18,11 @@ class ImGuiExample(ConanFile):
         toolchain.generate()
         dependencies.generate()
 
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options["boost"].without_stacktrace = "True"
+            self.options["boost"].with_stacktrace_backtrace = "False"
+        #if self.settings.os == "Macos":
     def layout(self):
         cmake_layout(self)
 
